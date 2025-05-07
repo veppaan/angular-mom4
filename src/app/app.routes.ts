eeeced568d08ee07e9825c63c3869bc7657b8a99
Vera Kippel <veki2400@student.miun.se>
2025-05-07 22:39:36 +0200

    länkar tom path till courses i routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 
 export const routes: Routes = [
+    { path: '', component: CoursesComponent },
     { path: 'courses', component: CoursesComponent }
 ];
