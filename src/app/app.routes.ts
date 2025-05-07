b7df978d75a35b4fe08f40867d504478f4b52264
Vera Kippel <veki2400@student.miun.se>
2025-05-07 14:11:55 +0200

    skapar komponent courses

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'courses', component: CoursesComponent }
+];
