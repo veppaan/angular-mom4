21b810f603d1c05fea2935568dc2048d9ce924bb
Vera Kippel <veki2400@student.miun.se>
2025-05-07 22:13:45 +0200

    lägger in styling i courses och börjar med sökning

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,21 +2,24 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CourseService } from '../services/course.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
   courseList: Course[] = [];
+  searchedCourses : Course[] = [];
 
   constructor(private courseservice : CourseService) {}
 
   ngOnInit() {
     this.courseservice.getCourses().subscribe(data => {
       this.courseList = data;
+      this.searchedCourses = data;
     })
   }
 }
